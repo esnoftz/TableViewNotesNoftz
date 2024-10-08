5211865fe8bfbd6e2040bb3565db9fddf52d3209
Evangeline Noftz <esnoftz@gmail.com>
2024-10-08 08:06:27 -0500

    Initial Commit.

--- TableViewNotesNoftz/ViewController.swift
+++ TableViewNotesNoftz/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+// adds protocols to the class
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    var cart = ["Apples", "Bananas", "Kiwi"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Don't forget to put identifier for the cell
+        
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
+        
     }
 
+    
+    
+    
+    
+    
+    
+    
+    // functions appear when you click "fix"
+    // these are the functions that are in UITableViewDataSource that have no code, and that must be implemented in this class to adopt the protocol
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // returns 3 rows
+        return cart.count
+        
+    }
+    // gets looped and called for each row (3 in this case bc there's 3 rows) -- called through the delegate process
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // "tableView" is a local variable (parameter!) -- kinda like "sender" in an action method
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = cart[indexPath.row]
+        cell.detailTextLabel?.text = "Hi"
+        return cell
+        
+    }
 
+    
+    
+    
+    
 }
 
