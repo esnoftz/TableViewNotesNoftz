a756710d1e36d9155b9354ece9e9f7ab6fac189a
Evangeline Noftz <esnoftz@gmail.com>
2024-10-08 07:27:52 -0500

    Initial Commit

---
+++ TableViewNotesNoftz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewNotesNoftz
+//
+//  Created by EVANGELINE NOFTZ on 10/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TableViewNotesNoftzTests/TableViewNotesNoftzTests.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewNotesNoftzTests.swift
+//  TableViewNotesNoftzTests
+//
+//  Created by EVANGELINE NOFTZ on 10/8/24.
+//
+
+import Testing
+@testable import TableViewNotesNoftz
+
+struct TableViewNotesNoftzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
